332883c15b41c7c21e58feab5468340bf28c4104
FranchescoSanchez <u00061c@ms.upla.edu.pe>
2026-09-20 11:39:54 -0500

    Update supabase.sql

--- supabase.sql
+++ supabase.sql
@@ -19,7 +19,7 @@ language sql
 stable
 set search_path = ''
 as $$
-  select auth.uid() = 'PEGA_AQUI_TU_UID'::uuid
+  select auth.uid() = 'f2d20dc1-ee26-47ac-8975-f0921e878b8c'::uuid
 $$;
 
 -- 2) Tabla de tareas ---------------------------------------------------
